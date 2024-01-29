a863b9a4edd4a75ff9aff7856d9759ce2a01500a
Sasha Makarava <4079864+SashaMkrv@users.noreply.github.com>
2024-01-29 16:58:22 -0500

    change text transform to single line
    you can do this in python too? neat.

--- Scripts/TextHandlers/TransformText.gd
+++ Scripts/TextHandlers/TransformText.gd
@@ -3,16 +3,12 @@ extends Node
 
 # process game text and return bbcode-ified text for use in game labels. 
 func transformText(text: String) -> String:
-	# Consider using regex to grab the contents of the brackets to chuck into meta tags
-	var newText = String(text)
 	
 	# if we don't, we can replace them close brackets dumb style.
 	# would wind up catching the close brackets from the bbcode as well.
-	newText = newText.replace("]", ">")
-	
-	
-	newText = newText.replace("[", "[b][color=yellow]")
-	newText = newText.replace(">", "[/color][/b]")
+	var newText := text.replace("]", ">") \
+	.replace("[", "[b][color=yellow]") \
+	.replace(">", "[/color][/b]")
 	
 	print_debug(newText)
 	return newText
