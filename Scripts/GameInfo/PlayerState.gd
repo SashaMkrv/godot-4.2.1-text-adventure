e35083ede944269aaec809b764af8ccb6631c667
Sasha Makarava <4079864+SashaMkrv@users.noreply.github.com>
2024-01-29 16:53:05 -0500

    move everything into folders
    they might not be ideal, but we'll figure it out as we go

---
+++ Scripts/GameInfo/PlayerState.gd
@@ -0,0 +1,4 @@
+class_name PlayerState
+extends Resource
+
+var inventory: Array[Resource]
---
+++ Scripts/GameInfo/Room.gd
@@ -0,0 +1,6 @@
+class_name Room
+extends Resource
+
+var description: String
+var exits: Array[Resource]
+var inventory: Array[Resource]
