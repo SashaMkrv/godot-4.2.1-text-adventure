cb3f2380e41e046858faf366d094f87cf3fb56ef
Sasha Makarava <4079864+SashaMkrv@users.noreply.github.com>
2024-01-29 22:24:25 -0500

    add factory for making actions
    the further i move from just making commands a verb and a noun in a wee refcounted object, the further i stray from god's light.

---
+++ Scripts/Actions/ActionFactory.gd
@@ -0,0 +1,33 @@
+class_name ActionFactory
+extends RefCounted
+
+var verb: StringName
+var specifier: StringName
+
+func setVerb(_verb: StringName) -> ActionFactory:
+    verb = _verb
+    return self
+
+func setSpecifier(_specifier: StringName) -> ActionFactory:
+    specifier = _specifier
+    return self
+
+
+func constructAction() -> PlayerAction:
+    if verb == null:
+        print_debug("You forgot the verb, friend")
+        return NoneAction.new(&"INVALID")
+    # TODO check if the specifier even handles the verb
+    # and while we're at it maybe just move the whole parsing shebang into here.
+    # a little "set text" or something as a catchall.
+    match verb:
+        &"GO":
+            return GoAction.new(specifier)
+        &"EAT":
+            return EatAction.new(specifier)
+        &"LOOK":
+            return LookAction.new(&"AROUND" if specifier == null else specifier)
+        &"USE":
+            return UseAction.new(specifier)
+        _:
+            return NoneAction.new(&"BAD VERB")
