2d6a12583e1b68b0ae8385d298f7f922d7d117dd
Sasha Makarava <4079864+SashaMkrv@users.noreply.github.com>
2024-10-08 23:49:24 -0400

    the mapplayer handles commands now
    along with everything else

--- NavigationOnly/Scripts/MapPlayer.gd
+++ NavigationOnly/Scripts/MapPlayer.gd
@@ -9,14 +9,44 @@ var gameText: TextEdit = %GameText
 @onready
 var commandInput: LineEdit = %CommandInput
 
-var currentItem: Item:
+@export
+var gameState: GameState
+@export
+var gameData: GameData
+
+var currentItem: GameItem:
 	set(value):
-		if currentItem == value:
-			return
+		# again, if the game wants to set the room, again. and run through it, go for it
+		# although I need some way to diff between immediately visitng
+		# and simply refreshing the screen
+		# separate instruction types?
+		#if currentItem == value:
+			#return
 		currentItem = value
 		changedItem()
 
 var _shortcutParser:= ShortcutParser.DefaultParser()
+var _commandParser:= CommandParser.new()
+
+# results of combining commands with items and state
+# print something
+# execute some kind of logic on state
+# move is a change in state, but also results in a thing being printed
+# AND clearing text.
+# So does looking around
+# so clear is a result
+# I guess results of commands are sequences of instructions?
+# clear screen, show this, change room
+# show this, remove from inv
+# show this, add to inv, change arbitrary var in state like so
+# hwo to track visited rooms?
+# keep a set of identifiers, I guess.
+# would have to keep track of vars as well
+# and where items are.
+# and player inventory.
+# eugh.
+# okay, no inventory, only scenery??
+# ONLY MOVEMENT RIGHT NOW.
 
 func changedItem() -> void:
 	if is_node_ready():
@@ -34,8 +64,15 @@ func resetText() -> void:
 
 func handlePlayerCommandSubmission(rawText: String) -> void:
 	commandInput.clear()
-	var text = rawText.strip_edges()
+	var text := rawText.strip_edges()
 	addCommandToGame(text)
+	var deshortcut := _shortcutParser.parse(text)
+	var instructions := _commandParser.parse(deshortcut, currentItem)
+	executeInstructions(instructions)
+
+func executeInstructions(instructions: Array[Instruction]) -> void:
+	for instruction in instructions:
+		instruction.executeChange(gameState)
 
 func addCommandToGame(command: String) -> void:
 	gameText.text += "\n> " + command
@@ -81,11 +118,16 @@ func colorTitle() -> void:
 func setGameTextToItem() -> void:
 	gameText.text = currentItem.description
 
+func roomChanged() -> void:
+	currentItem = gameData.items[gameState.currentRoom]
 
 func _ready() -> void:
-	var item := Item.EmptyItem()
-	item.flavorColor = Color.BLACK
-	item.description = "example description! Things are neat."
-	item.displayName = "Room Display Name"
-	currentItem = item
+	if gameData == null:
+		printerr("We are out of luck.")
+	if gameState == null:
+		gameState = GameState.new()
+		gameState.currentRoom = gameData.startRoom
+	
+	gameState.current_room_changed.connect(roomChanged)
+	currentItem = gameData.items[gameState.currentRoom]
 	updateForItem()
