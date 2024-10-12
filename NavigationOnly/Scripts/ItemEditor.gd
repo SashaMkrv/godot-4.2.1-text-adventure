8f7932174a392ac82931aef01354e405a077031c
Sasha Makarava <4079864+SashaMkrv@users.noreply.github.com>
2024-10-11 21:23:35 -0400

    add aliases to editor, handle big text generically

--- NavigationOnly/Scripts/ItemEditor.gd
+++ NavigationOnly/Scripts/ItemEditor.gd
@@ -6,7 +6,7 @@ var item: Item:
 	set(value):
 		if item == value:
 			return
-		saveBigFields()
+		savePendingBigFields()
 		item = value
 		newItemWasSet()
 
@@ -18,19 +18,62 @@ var descriptionInput: TextEdit = %DescriptionInput
 var uniqueNameInput: LineEdit = %ReferenceStringInput
 @onready
 var displayNameInput: LineEdit = %DisplayNameInput
+
 @onready
 var connectionsInput: TextEdit = %ConnectionsInput
 @onready
 var sceneryInput: TextEdit = %SceneryInput
+@onready
+var aliasInput: TextEdit = %AliasInput
 
 @onready
 var largeSaveTimer: Timer = $Timer
 
+class BigTextItem:
+	var waiting: bool = false
+	# TODO remove this input, and remove the @onready on the field definitions
+	var input: TextEdit
+	var setItem: Callable
+	var getItem: Callable
+	func _init(_waiting: bool, _inputNode: TextEdit, textSetter: Callable, textGetter: Callable) -> void:
+		waiting = waiting
+		input = _inputNode
+		setItem = textSetter
+		getItem = textGetter
+	func save() -> void:
+		setItem.call(getItem.call())
 
-# move out to an array of things waiting to save?
-var _descriptionWaitingForSave: bool = false
-var _connectionsWaitingForSave: bool = false
-var _sceneryWaitingForSave: bool = false
+@onready
+var descriptionItem: BigTextItem = BigTextItem.new(
+	false,
+	descriptionInput,
+	updateDescription,
+	getDescriptionText
+)
+@onready
+var connectionsItem: BigTextItem = BigTextItem.new(
+	false,
+	connectionsInput,
+	updateConnections,
+	getConnectionsText
+)
+@onready
+var sceneryItem: BigTextItem = BigTextItem.new(
+	false,
+	sceneryInput,
+	updateScenery,
+	getSceneryText
+)
+@onready
+var aliasItem: BigTextItem = BigTextItem.new(
+	false,
+	aliasInput,
+	updateAliases,
+	getAliasText
+)
+
+# ohhh how i would like a set....
+var pendingSaveQueue: Array[BigTextItem]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -53,64 +96,40 @@ func updateFields() -> void:
 		displayNameInput.text = item.displayName
 		connectionsInput.text = item.connectionsScript
 		sceneryInput.text = item.sceneryScript
+		aliasInput.text = item.aliasScript
 
 
 # BIG TEXT/SCRIPT UPDATE FUNCTIONS
 
-func descriptionHasChanged() -> void:
-	_descriptionWaitingForSave = true
+func addScriptToSaveQueue(script: BigTextItem) -> void:
+	if pendingSaveQueue.has(script):
+		return
+	pendingSaveQueue.append(script)
 	if largeSaveTimer.is_stopped():
 		largeSaveTimer.start()
 
-func forceDescriptionSave() -> void:
-	updateDescription(getDescriptionText())
-	
-func descriptionSave() -> void:
-	if _descriptionWaitingForSave:
-		updateDescription(getDescriptionText())
-		_descriptionWaitingForSave = false
+func savePendingBigFields() -> void:
+	for item in pendingSaveQueue:
+		item.save()
+	pendingSaveQueue.clear()
+	# so that when saving on item switch doesn't leave a timer running.
+	largeSaveTimer.stop()
+
+func forceSave(script: BigTextItem) -> void:
+	item.save()
+
 
 func getDescriptionText() -> String:
 	return descriptionInput.text
 
-
-func connectionsHasChanged() -> void:
-	_connectionsWaitingForSave = true
-	if largeSaveTimer.is_stopped():
-		largeSaveTimer.start()
-
-func forceConnectionsSave() -> void:
-	updateConnections(getConnectionsText())
-
-func connectionsSave() -> void:
-	if _connectionsWaitingForSave:
-		updateConnections(getConnectionsText())
-		_connectionsWaitingForSave = false
-
 func getConnectionsText() -> String:
 	return connectionsInput.text
-
-
-func sceneryHasChanged() -> void:
-	_sceneryWaitingForSave = true
-	if largeSaveTimer.is_stopped():
-		largeSaveTimer.start()
-
-func forceScenerySave() -> void:
-	updateScenery(getSceneryText())
-
-func scenerySave() -> void:
-	if _sceneryWaitingForSave:
-		updateScenery(getSceneryText())
-		_sceneryWaitingForSave = false
 
 func getSceneryText() -> String:
 	return sceneryInput.text
 
-
-## TODO I'd like to say I've reached enough of these fields to start abstracting
-## and for simple save-as-script-text fields I think I can?
-## but *not* anything other than the saving
+func getAliasText() -> String:
+	return aliasInput.text
 
 
 func updateDescription(newValue: String) -> void:
@@ -121,6 +140,10 @@ func updateConnections(newValue: String) -> void:
 
 func updateScenery(newValue: String) -> void:
 	item.sceneryScript = newValue
+
+func updateAliases(newValue: String) -> void:
+	item.aliasScript = newValue
+
 
 func updateUniqueName(newValue: String) -> void:
 	# worth it to check if there's a duplicate and stop the change?
@@ -133,15 +156,9 @@ func updateMapCellColor(newValue: Color) -> void:
 	item.flavorColor = newValue
 
 
-func saveBigFields() -> void:
-	descriptionSave()
-	connectionsSave()
-	scenerySave()
-
 
 func _on_map_cell_color_input_color_changed(color: Color) -> void:
 	updateMapCellColor(color)
-
 
 
 func _on_reference_string_input_text_changed(new_text: String) -> void:
@@ -152,19 +169,20 @@ func _on_display_name_input_text_changed(new_text: String) -> void:
 	updateDisplayName(new_text)
 
 
+func _on_timer_timeout() -> void:
+	savePendingBigFields()
+
+
 # BIG TEXT/SCRIPT ITEM CONNECTIONS
 func _on_description_input_text_changed() -> void:
 	# not immediate. This can be a little chunkier and changes more often.
-	descriptionHasChanged()
-
+	addScriptToSaveQueue(descriptionItem)
 
 func _on_connections_input_text_changed() -> void:
-	connectionsHasChanged()
-
+	addScriptToSaveQueue(connectionsItem)
 
 func _on_scenery_input_text_changed() -> void:
-	sceneryHasChanged()
+	addScriptToSaveQueue(sceneryItem)
 
-
-func _on_timer_timeout() -> void:
-	saveBigFields()
+func _on_alias_input_text_changed() -> void:
+	addScriptToSaveQueue(aliasItem)
