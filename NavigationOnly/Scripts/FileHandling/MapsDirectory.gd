9bbfb22fa3413d31c7f1e4b54547c30522c4864e
Sasha Makarava <4079864+SashaMkrv@users.noreply.github.com>
2024-11-01 19:27:55 -0400

    updates to files I don't use???
    could use them to index names and description, but that would turn it into a different source of truth

--- NavigationOnly/Scripts/FileHandling/MapsDirectory.gd
+++ NavigationOnly/Scripts/FileHandling/MapsDirectory.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name MapsDirectory
 
-const FILE_PATH:= "user://directory.txt"
+const FILE_PATH:= "user://map-directory.json"
 const mapsKey:= "savedMaps"
 
 
@@ -15,7 +15,7 @@ var savedMaps: Array[SavedFile] = []
 
 func hasFile(savedFile: SavedFile) -> bool:
 	for file in savedMaps:
-		if file.filename == savedFile.filename:
+		if SavedFile.equivalent(file, savedFile):
 			return true
 	return false
 
@@ -26,9 +26,18 @@ func addFile(savedFile: SavedFile) -> void:
 	savedMaps.append(savedFile)
 	_overwriteDirectory(self)
 
+func removeFile(savedFile: SavedFile) -> void:
+	if not hasFile(savedFile):
+		printerr("trying to remove unknown file")
+		return
+	print("removing file from directory")
+	savedMaps = savedMaps.filter(SavedFile.notEquivalent.bind(savedFile))
+	_overwriteDirectory(self)
+
 static func _overwriteDirectory(directory: MapsDirectory) -> void:
 	var dirString = str(directory)
 	var writeFile = FileAccess.open(FILE_PATH, FileAccess.WRITE)
+	print("saving map directory:")
 	print(dirString)
 	writeFile.store_string(dirString)
 	writeFile.close()
@@ -40,16 +49,17 @@ static func getOrCreateMapsDirectory() -> MapsDirectory:
 
 static func readDirectoryFrom(filepath: String) -> MapsDirectory:
 	var fileString = FileAccess.get_file_as_string(FILE_PATH)
-	return _parseDirectoryFromString(fileString)
+	var parsed := _parseDirectoryFromString(fileString)
+	print("read map directory:")
+	print(parsed)
+	return parsed
 
 static func _createDirectoryFile() -> void:
 	var emptyDirectory = MapsDirectory.new()
 	_overwriteDirectory(emptyDirectory)
 
 static func _parseDirectoryFromString(string: String) -> MapsDirectory:
-	print(string)
 	var readFile = JSON.parse_string(string)
-	print(readFile)
 	if not typeof(readFile) == TYPE_DICTIONARY:
 		printerr("maps directory is of unexpected type")
 		return null
@@ -66,7 +76,7 @@ static func _parseDirectoryFromString(string: String) -> MapsDirectory:
 	
 	var savedMapsRawArray: Array = savedMapsValue
 	var maps = savedMapsRawArray.map(
-		SavedFile.savedFileFromString
+		SavedFile.savedFileFromDictionary
 	).filter(
 		func(item): return item != null
 	)
@@ -74,15 +84,13 @@ static func _parseDirectoryFromString(string: String) -> MapsDirectory:
 	var directory = MapsDirectory.new()
 	var mapsTyped: Array[SavedFile]
 	mapsTyped.assign(maps)
-	print(mapsTyped)
 	directory.savedMaps = mapsTyped
-	print(directory)
 	return directory
 
 func _to_string() -> String:
 	# please, typed map.
 	var savedFileStringsArray: Array = savedMaps.map(
-		str
+		func (savedMap): return savedMap._to_dict()
 	)
 	var directoryDictionary := {
 		mapsKey: savedFileStringsArray
--- NavigationOnly/Scripts/FileHandling/SavedFile.gd
+++ NavigationOnly/Scripts/FileHandling/SavedFile.gd
@@ -9,20 +9,20 @@ var filename: String # this acts as the identifier
 func _init(rawfilename: String = "") -> void:
 	filename = rawfilename
 
-func _to_string() -> String:
-	var dict := {
+func _to_dict() -> Dictionary:
+	return {
 		filenameKey: filename
 	}
-	return str(dict)
 
-static func savedFileFromString(string: String) -> SavedFile:
-	var parsedString = JSON.parse_string(string)
-	print(parsedString)
-	if not typeof(parsedString) == TYPE_DICTIONARY:
-		printerr("unexpected format for saved file")
-		return null
-	
-	var dict: Dictionary = parsedString
+func _to_string() -> String:
+	return str(self._to_dict())
+
+static func equivalent(savedFile1: SavedFile, savedFile2: SavedFile) -> bool:
+	return savedFile1.filename == savedFile2.filename
+static func notEquivalent(savedFile1: SavedFile, savedFile2: SavedFile) -> bool:
+	return not equivalent(savedFile1, savedFile2)
+
+static func savedFileFromDictionary(dict: Dictionary) -> SavedFile:
 	if not dict.has(filenameKey):
 		printerr("no file name in saved file")
 		return null
@@ -33,3 +33,13 @@ static func savedFileFromString(string: String) -> SavedFile:
 		return null
 	
 	return SavedFile.new(filename)
+
+static func savedFileFromString(string: String) -> SavedFile:
+	var parsedString = JSON.parse_string(string)
+	print(parsedString)
+	if not typeof(parsedString) == TYPE_DICTIONARY:
+		printerr("unexpected format for saved file")
+		return null
+	
+	var dict: Dictionary = parsedString
+	return savedFileFromDictionary(dict)
